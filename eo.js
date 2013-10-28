7d813f25323a95a20f940d20ba9c968e4639f962
Marat Abdullin <dakota@brokenpipe.ru>
2013-10-29 01:13:10 +0400

    Initial commit.

---
+++ eo.js
@@ -0,0 +1,83 @@
+/*!
+ * EO.js v0.0.0+, https://github.com/hoho/eo
+ * (c) 2013 Marat Abdullin, MIT license
+ */
+
+var $EO = function() {
+    var self = this,
+        data = {},
+        handlers = {},
+        tmp,
+        tmp2,
+        i;
+
+    self.on = function(name, callback) {
+        (handlers[name] || (handlers[name] = [])).push(callback);
+
+        return self;
+    };
+
+    self.off = function(name, callback) {
+        if ((tmp = handlers[name])) {
+            if (callback) {
+                i = 0;
+                while (i < tmp.length) {
+                    if (tmp[i] === callback) {
+                        tmp.splice(i, 1);
+                    } else {
+                        i++;
+                    }
+                }
+            }
+
+            if (!callback || !tmp.length) {
+                delete handlers[name];
+            }
+        }
+
+        return self;
+    };
+
+    self.set = function(name, val) {
+        tmp = data[name];
+        data[name] = val;
+
+        if ((tmp2 = handlers[name])) {
+            for (i = 0; i < tmp2.length; i++) {
+                tmp2[i].call(self, val, tmp, name);
+            }
+        }
+
+        return self;
+    };
+
+    self.get = function(name) {
+        return data[name];
+    };
+};
+
+
+$EO.extend = function(obj) {
+    var self = this,
+        proto,
+        key,
+
+        F = function() {},
+
+        Child = function() {
+            var s = this;
+            $EO.call(s);
+            s.init && s.init.apply(s, Array.prototype.slice.call(arguments, 0));
+        };
+
+    F.prototype = self.prototype;
+    proto = Child.prototype = new F;
+    proto.constructor = Child;
+    for (key in obj) {
+        proto[key] = obj[key];
+    }
+    Child.extend = $EO.extend;
+    Child.super = self.prototype;
+
+    return Child;
+};
